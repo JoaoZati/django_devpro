6e735adacd5e175ba32a74ff4d51837e450cba7d
JoaoZati <joaozati@gmail.com>
2021-11-05 13:45:25 -0300

    Part 6.2: add bitcoin payment. - Pipfile: install django-qr-code, forex-python, requests; - home.html: add qr code, button payment and BTC price; - bitcoin.py: function return btc price; - settings.py: add qr_code into INSTALLED_APPS; - views.py: add context for btc_price;

--- base/views.py
+++ base/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from base.bitcoin import convert_brl_btc
+
 
 def home(request):
-    return render(request, 'base/home.html')
+    btc_price = convert_brl_btc(1000)
+    context = {
+        'btc_price': btc_price,
+    }
+    return render(request, 'base/home.html', context)
