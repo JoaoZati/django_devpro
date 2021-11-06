96ad39045bd7f77db201271ed68fefe58a22980b
JoaoZati <joaozati@gmail.com>
2021-11-06 17:07:15 -0300

    Part 8: add test video_not found: - test_demonstration.py: add resp video not found adn test status_code == 404;

--- demonstration/tests/test_demonstration.py
+++ demonstration/tests/test_demonstration.py
@@ -25,3 +25,13 @@ def test_title_video(resp, video):
 
 def test_content_video(resp, video):
     assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}')
+
+
+@pytest.fixture
+def resp_video_not_found(client, video):
+    print('resp')
+    return client.get(reverse('demonstration:video', args=(video.slug+'random_content',)))
+
+
+def test_status_code_video_not_found(resp_video_not_found):
+    assert resp_video_not_found.status_code == 404
