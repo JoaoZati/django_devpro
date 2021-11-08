2133eff2aae97f2edfc00b99480246ee9e2c56e8
JoaoZati <joaozati@gmail.com>
2021-11-08 14:11:41 -0300

    Part 10: Create tests for lessons details; - test_lessons_details.py: create test_title and test_vimeo in the app;

---
+++ modules/tests/test_lessons_details.py
@@ -0,0 +1,30 @@
+import pytest
+from django.urls import reverse
+from model_mommy import mommy
+
+from main.django_assertions import assert_contains
+from modules.models import Lesson, Module
+
+
+@pytest.fixture
+def module(db):
+    return mommy.make(Module)
+
+
+@pytest.fixture
+def lesson(module):
+    return mommy.make(Lesson, module=module)
+
+
+@pytest.fixture
+def resp(client, lesson):
+    resp = client.get(reverse('modules:lesson', kwargs={'slug': lesson.slug}))
+    return resp
+
+
+def test_title(resp, lesson):
+    assert_contains(resp, lesson.title)
+
+
+def test_vimeo(resp, lesson):
+    assert_contains(resp, f'src="https://player.vimeo.com/video/{ lesson.vimeo_id }"')
