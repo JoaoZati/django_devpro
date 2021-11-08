f6422d5f12231ece6794b05814910fc622198adb
JoaoZati <joaozati@gmail.com>
2021-11-08 17:37:48 -0300

    Part 12: Create Class model with many to many relationship; - models.py: create class Class and class Registration; - admin.py: create ClassAdmin using RegistrationInLine; - 0001_initial.py: makemigration file;

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,20 @@
-from django.db import models # noqa
+from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Class(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64)
+    start = models.DateField()
+    end = models.DateField()
+    students = models.ManyToManyField(get_user_model(), through='Registration')
+
+
+class Registration(models.Model):
+    data = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    class_user = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [['user', 'class_user']]
+        ordering = ['class_user', 'data']
