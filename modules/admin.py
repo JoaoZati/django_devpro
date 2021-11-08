28298a8af389b4f6df399cfa39222e5ebd850bd3
JoaoZati <joaozati@gmail.com>
2021-11-08 11:52:26 -0300

    Part 10: Create Lesson model and LessonAdmin; - models.py: create Lesson Model; - admin.py: create LessonAdmin;

--- modules/admin.py
+++ modules/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
-from modules.models import Module
+from modules.models import Module, Lesson
 
 
 @admin.register(Module)
 class ModuloAdmin(OrderedModelAdmin):
     list_display = ('title', 'slug', 'public', 'move_up_down_links')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Lesson)
+class LessonAdmin(OrderedModelAdmin):
+    list_display = ('title', 'module', 'slug', 'move_up_down_links')
+    list_filter = ('module',)
+    ordering = ('module', 'slug')
     prepopulated_fields = {'slug': ('title',)}
